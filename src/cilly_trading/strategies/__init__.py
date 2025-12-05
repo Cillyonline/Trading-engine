a9f025bce4463a9f9b7cf1971e31cf10c9449cd1
Cillyonline <cilly2510@googlemail.com>
2025-12-05 22:23:47 +0100

    Add TurtleStrategy to strategies package

--- src/cilly_trading/strategies/__init__.py
+++ src/cilly_trading/strategies/__init__.py
@@ -3,11 +3,12 @@ Strategie-Paket für die Cilly Trading Engine.
 
 Im MVP enthalten:
 - Rsi2Strategy (Rebound auf Basis RSI2)
-- (später) TurtleStrategy
+- TurtleStrategy (Breakout über N-Tage-Hoch)
 """
 
 from __future__ import annotations
 
 from .rsi2 import Rsi2Strategy
+from .turtle import TurtleStrategy
 
-__all__ = ["Rsi2Strategy"]
+__all__ = ["Rsi2Strategy", "TurtleStrategy"]
